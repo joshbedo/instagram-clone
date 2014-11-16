3cbb760fa8f1d877dcbd5b67439817229b07c875
Josh Bedo <joshbedo@yahoo.com>
2014-11-16 00:40:12 -0500

    Adding twilio rest client, adding sendPhoto method, handling tmp file, applying filter

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
 var Hapi = require('hapi'),
     Joi  = require('joi'),
-    Boom = require('boom');
+    Boom = require('boom'),
+    uuid = require('node-uuid'),
+    os   = require('os'),
+    fs   = require('fs'),
+    request = require('request'),
+    Caman   = require('caman').Caman,
+    twilio  = require('twilio'),
+    twilioClient = new twilio.RestClient();
+
+// TODO: Going to move this to a filters.js module once its working
+var filters = [
+  'vintage',
+  'logo',
+  'clarity',
+  'sinCity',
+  'sunrise',
+  'crossProcess',
+  'orangePeel',
+  'love',
+  'grungy',
+  'jarques',
+  'pinhole',
+  'oldBoot',
+  'glowingSun',
+  'hazyDays',
+  'herMajesty',
+  'nostalgia',
+  'hemingway',
+  'concentrate'
+];
+
+// Download and apply filter to photo
+function applyFilter(mediaUrl, filter, from, to, url_base) {
+  // create a unique id for our video/gif processing
+  var id = uuid.v1(),
+      original = os.tmpdir() + '/' + id,
+      filtered = id + '.png',
+      filteredPath = './static/' + filtered;
+  
+  // save the remote image file to /tmp fs
+  download = request(mediaUrl).pipe(fs.createWriteStream(original));
+  
+  download.on('finish', function() {
+    // initialize CamanJS
+    Caman(original, function() {
+      this.resize({ width: 600 });
+      this[filter]();
+      this.render(function() {
+        // save to the fs
+        this.save(filteredPath);
+        console.log('Saved: ', filtered);
+        // delete tmp file
+        fs.unlink(original, function(err) { console.log(err); });
+        sendPhoto( url_base, filtered, from, to );
+      });
+    });
+  });
+
+}
 
 // Schema validation for payload, probably move this into a module later
 
@@ -25,20 +83,48 @@ function handleMessage(req, reply) {
       to = req.payload.To,
       mediaUrl = req.payload.MediaUrl0,
       mediaContentType = req.payload.MediaContentType0,
-      filter = req.payload.Body.toLowerCase().trim();
+      filter = req.payload.Body.toLowerCase().trim(),
+      // twim1 object for sending replies to users
+      twim1 = new twilio.Twim1Response();
   
   console.log('Processing MMS: ', mediaUrl, mediaContentType, filter);
+  
+  // verify that the filter exists in the list
+  var filterValid = false;
+  
+  if( filters.indexOf( filter.toLowerCase() ) !== -1 ) {
+    filterValid = true;
+    filter = filters.indexOf( filter.toLowerCase() );
+  }
 
   // check to see if the user has submitted an image and filter
   if( mediaUrl && mediaContentType && mediaContentType.indexOf('image') !== 0 ) {
-    if( filter ) {
+    if( filterValid ) {
      // send `hang tight! working on it` message
+     twim1.message('Hang tight! Applying filter');
+     reply(twim1.toString()).type('text/xml');
+
+     applyFilter( mediaUrl, filter, from, to, url_base );
     } else {
      // respond with a list of valid filters
     }
   } else {
     // send instructions for the app
   }
+}
+
+// Send an MMS message with filtered photo attached
+function sendPhoto(url_base, photo, from, to) {
+  var photoUrl = url_base + '/' + photo;
+  
+  twilioClient.sendMessage({
+    to: from, from: to,
+    body: 'Powered by Twilio MMS',
+    mediaUrl: photoUrl
+  }, function( err, responseData ) {
+    if (err) { console.log('Error sending MMS: ', JSON.stringify(err) ); }
+  }
+  });
 }
 
 // Create a server with a host and port
